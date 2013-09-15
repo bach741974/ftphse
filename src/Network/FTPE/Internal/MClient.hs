036fced9df88a47370b59077f05609782bbeec3d
bahvalov <bahvalov@rocketsoftware.com>
2013-09-16 01:28:23 +0600

    rewrote sendcmdM, cwdM

--- src/Network/FTPE/Internal/MClient.hs
+++ src/Network/FTPE/Internal/MClient.hs
@@ -10,11 +10,9 @@ import qualified Network.FTP.Client  as N
 
 import Network.FTP.Client.Parser (FTPResult)
 
---import Control.Monad.STM (atomically)
+
 import Control.Concurrent.STM.TMVar
---import Control.Concurrent (forkFinally, threadDelay)
--- import GHC.Conc.Sync (ThreadId)
---import Control.Monad (forever)
+
 import Control.Monad.State.Strict (runStateT, StateT, liftIO, get)
 
 
@@ -27,13 +25,9 @@ quitM :: FTPM FTPResult
 quitM = monblock' $ flip block' N.quit
 
 sendcmdM, cwdM:: String -> FTPM FTPResult                  
-sendcmdM = sM'' N.sendcmd
-cwdM = sM'' N.cwd
- 
---sM'' :: (MonadIO m, MonadState (TMVar FConnection) m) =>
---          (N.FTPConnection -> b1 -> IO b) -> b1 -> m b
-sM'' ::(N.FTPConnection -> b1 -> IO b) -> b1 -> FTPM b
-sM'' fun str = monblock' (\s ->  block' s  $ flip fun str)
+sendcmdM s = monblock' $ flip sendcmd s
+cwdM s = monblock' $ flip cwd s
+
 
 monblock' :: 
                (TMVar FConnection -> IO b) -> FTPM b
